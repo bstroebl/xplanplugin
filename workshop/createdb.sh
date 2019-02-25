ea8d851df81794063bf35b1c9352914600c79b61
Bernhard Ströbl <bernhard.stroebl@jena.de>
2019-02-25 14:41:57 +0100

    [workshop] Korrigiere Verzeichnis

--- workshop/createdb.sh
+++ workshop/createdb.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 createdb xplan
 psql xplan -c "create extension postgis"
-psql xplan -a -f /home/user/Downloads/XP_Basisschema.sql > XP_Basisschema.log 2>&1
-psql xplan -a -f /home/user/Downloads/BP_Fachschema_BPlan.sql > BP_Fachschema_BPlan.log 2>&1
-psql xplan -a -f /home/user/Downloads/FP_Fachschema_FPlan.sql > FP_Fachschema_FPlan.log 2>&1
-psql xplan -a -f /home/user/Downloads/LP_Fachschema_LPlan.sql > LP_Fachschema_LPlan.log 2>&1
-psql xplan -a -f /home/user/Downloads/RP_Fachschema_Regionalplan.sql > RP_Fachschema_Regionalplan.log 2>&1
-psql xplan -a -f /home/user/Downloads/SO_Fachschema_SonstigePlaene.sql > SO_Fachschema_SonstigePlaene.log 2>&1
-psql xplan -a -f /home/user/Downloads/QGIS.sql > QGIS.log 2>&1
-psql xplan -a -f /home/user/Downloads/XP_QGIS.sql > XP_QGIS.log 2>&1
-psql xplan -a -f /home/user/Downloads/BP_QGIS.sql > BP_QGIS.log 2>&1
-psql xplan -a -f /home/user/Downloads/FP_QGIS.sql > FP_QGIS.log 2>&1
-psql xplan -a -f /home/user/Downloads/LP_QGIS.sql > LP_QGIS.log 2>&1
-psql xplan -a -f /home/user/Downloads/SO_QGIS.sql > SO_QGIS.log 2>&1
-psql xplan -a -f /home/user/Downloads/layer_styles_QGIS.sql > layer_styles_QGIS.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/XP_Basisschema.sql > XP_Basisschema.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/BP_Fachschema_BPlan.sql > BP_Fachschema_BPlan.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/FP_Fachschema_FPlan.sql > FP_Fachschema_FPlan.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/LP_Fachschema_LPlan.sql > LP_Fachschema_LPlan.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/RP_Fachschema_Regionalplan.sql > RP_Fachschema_Regionalplan.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/SO_Fachschema_SonstigePlaene.sql > SO_Fachschema_SonstigePlaene.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/QGIS.sql > QGIS.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/XP_QGIS.sql > XP_QGIS.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/BP_QGIS.sql > BP_QGIS.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/FP_QGIS.sql > FP_QGIS.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/LP_QGIS.sql > LP_QGIS.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/SO_QGIS.sql > SO_QGIS.log 2>&1
+psql xplan -a -f /home/user/Downloads/xplanPostGIS-5.1/layer_styles_QGIS.sql > layer_styles_QGIS.log 2>&1
